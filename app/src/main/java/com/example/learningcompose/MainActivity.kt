92c30699bc35dbdb874dbd8057a2cff8600d4d6d
MSI KATANA <gaudentas199@gmail.com>
2023-03-24 11:04:05 +0530

    Added initial taost message

--- app/src/main/java/com/example/learningcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningcompose/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.learningcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.learningcompose.ui.theme.LearningComposeTheme
 
@@ -31,8 +38,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column() {
+        Text(text = "Hello $name!")
+        CTA()
+    }
 }
+
+@Composable
+fun CTA() {
+
+    val context = LocalContext.current
+
+    Button(onClick = {
+        Toast.makeText(context, "Hey there", Toast.LENGTH_SHORT).show()
+    }) {
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
