7377376d481436259d54f9c2001224ea5c09f227
MSI KATANA <gaudentas199@gmail.com>
2023-03-24 11:32:14 +0530

    Initial setup done

--- app/src/main/java/com/example/learningcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningcompose/MainActivity.kt
@@ -5,15 +5,15 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,7 +41,46 @@ fun Greeting(name: String) {
     Column() {
         Text(text = "Hello $name!")
         CTA()
+        InputField()
+        ExpandableCard("Initial Title", "Initial Description")
     }
+}
+
+@Composable
+fun ExpandableCard(title: String, description: String) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    Card {
+        Column {
+            Text(text = title)
+
+            if(expanded) {
+                Text(text = description)
+                IconButton(onClick = {
+                    expanded = false
+                }) {
+                }
+            } else {
+                IconButton(onClick = {
+                    expanded = true
+                }) {
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun InputField() {
+    var name = "String"
+
+    TextField(value = "", onValueChange = {
+        name = it
+    }, label = { Text(text = "Id here")})
+
 }
 
 @Composable
