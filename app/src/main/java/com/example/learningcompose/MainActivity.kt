6c598238d9dc1af112103afedde18dd5be76497f
MSI KATANA <gaudentas199@gmail.com>
2023-04-10 05:20:10 +0530

    learnt new about alignment and arrangement

--- app/src/main/java/com/example/learningcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningcompose/MainActivity.kt
@@ -4,23 +4,17 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.learningcompose.ui.theme.LearningComposeTheme
 import com.example.learningcompose.ui.theme.Typography
-import kotlin.text.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,8 +23,7 @@ class MainActivity : ComponentActivity() {
             LearningComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
                 }
@@ -40,12 +33,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Column() {
-        Text(text = "Hello $name!")
-        CTA()
-        InputField()
-        ExpandableCard("Initial Title", "Initial Description")
+fun Greeting(
+    name: String,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Surface(
+            modifier = Modifier
+                .width(200.dp)
+                .height(200.dp), color = Color.Red
+        ) {}
+        Surface(
+            modifier = Modifier
+                .width(200.dp)
+                .height(200.dp), color = Color.Green
+        ) {}
+        Surface(
+            modifier = Modifier
+                .width(200.dp)
+                .height(200.dp), color = Color.Blue
+        ) {}
+
     }
 }
 
@@ -55,7 +66,9 @@ fun ExpandableCard(title: String, description: String) {
         mutableStateOf(false)
     }
 
-    Card {
+    Card(
+        modifier = Modifier.fillMaxSize()
+    ) {
         Column {
             Text(text = title)
 
@@ -65,13 +78,11 @@ fun ExpandableCard(title: String, description: String) {
                 )
                 IconButton(onClick = {
                     expanded = false
-                }) {
-                }
+                }) {}
             } else {
                 IconButton(onClick = {
                     expanded = true
-                }) {
-                }
+                }) {}
             }
         }
     }
@@ -86,8 +97,7 @@ fun InputField() {
         name = it
     }, label = {
         Text(
-            text = "Id here",
-            style = Typography.h3
+            text = "Id here", style = Typography.h3
 
         )
     })
