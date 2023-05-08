28d0048525640338a36a3792f81be15b65c55180
MSI KATANA <gaudentas199@gmail.com>
2023-05-08 10:18:36 +0530

    Codelab lesson 6

--- app/src/main/java/com/example/learningcompose/CodeLabSampleActivity.kt
+++ app/src/main/java/com/example/learningcompose/CodeLabSampleActivity.kt
@@ -3,8 +3,12 @@ package com.example.learningcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -37,8 +41,37 @@ private fun MyApp(modifier: Modifier = Modifier) {
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.secondary) {
-        Text(text = "Hello $name!")
+    Surface(
+        color = MaterialTheme.colors.secondary,
+        modifier = Modifier.padding(
+            vertical = 4.dp,
+            horizontal = 8.dp
+        )
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Show More")
+            }
+        }
+    }
+}
+
+@Composable
+fun LoopingText(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose", "Mahmood"),
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp, horizontal = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
@@ -46,6 +79,8 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview2() {
     LearningComposeTheme {
-        MyApp()
+        LoopingText(
+            modifier = Modifier
+        )
     }
 }
