156dcbbcb1aec59cb8873647092ecb5f4bfc9efb
MSI KATANA <gaudentas199@gmail.com>
2023-07-06 13:24:49 +0530

    Selectables working

--- app/src/main/java/com/example/learningcompose/selectables/Survey.kt
+++ app/src/main/java/com/example/learningcompose/selectables/Survey.kt
@@ -15,9 +15,11 @@ import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -35,8 +37,8 @@ import kotlinx.coroutines.delay
 @Composable
 fun Survey(listOf: List<Answer>) {
 
-    val selectedAnswer: MutableState<Answer?> = rememberSaveable {
-        mutableStateOf(null)
+    var selectedAnswer by rememberSaveable {
+        mutableStateOf<Answer?>(null)
     }
 
     Column(
@@ -56,9 +58,9 @@ fun Survey(listOf: List<Answer>) {
                 key(answer.id) {
                     SurveyAnswer(
                         answer = answer,
-                        isSelected = selectedAnswer.value == answer
+                        isSelected = selectedAnswer == answer
                     ) {
-                        selectedAnswer.value = it
+                        selectedAnswer = it
                     }
                 }
             }
@@ -102,16 +104,7 @@ fun SurveyAnswer(
             }
         )
     }
-    Row {
-        Column {
-            Thread.sleep(1000)
-            Box {
-                Row {
 
-                }
-            }
-        }
-    }
 }
 
 
