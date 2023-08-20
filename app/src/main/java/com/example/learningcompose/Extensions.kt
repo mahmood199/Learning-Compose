29baf78f0dca56bc024f9a17ed07ed43c6343b3d
Mohammed Mahmood Ahmed <gaudentas199@gmail.com>
2023-08-20 13:58:33 +0530

    Explosion Animation added

--- app/src/main/java/com/example/learningcompose/Extensions.kt
+++ app/src/main/java/com/example/learningcompose/Extensions.kt
@@ -1,5 +1,12 @@
 package com.example.learningcompose
 
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 
 fun LazyListState.isScrolledToTheLast() = layoutInfo.visibleItemsInfo.lastOrNull()?.index == layoutInfo.totalItemsCount - 1
+
+
+@Composable
+fun Dp.toPx() = with(LocalDensity.current) { this@toPx.toPx() }
---
+++ app/src/main/java/com/example/learningcompose/animation/explosion/ExplosionAnimation.kt
@@ -0,0 +1,110 @@
+package com.example.learningcompose.animation.explosion
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Slider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.learningcompose.toPx
+import com.example.learningcompose.ui.theme.LearningComposeTheme
+
+@Composable
+fun ControlledExplosion() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        var progress by remember {
+            mutableStateOf(0f)
+        }
+
+        Explosion(progress)
+
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(32.dp)
+        )
+
+        Slider(
+            modifier = Modifier.fillMaxWidth(0.5f),
+            value = progress,
+            onValueChange = { progress = it }
+        )
+
+    }
+}
+
+@Composable
+fun Explosion(progress: Float) {
+    val sizeDp = 200.dp
+    val sizePx = sizeDp.toPx()
+    val sizePxHalf = sizePx / 2
+    val particles = remember {
+        List(150) {
+            Particle(
+                color = listOf(Color.Red, Color.Blue, Color.Green, Color.Yellow).random(),
+                startXPosition = sizePxHalf.toInt(),
+                startYPosition = sizePxHalf.toInt(),
+                maxHorizontalDisplacement = sizePx * randomInRange(-0.9f, 0.9f),
+                maxVerticalDisplacement = sizePx * randomInRange(0.2f, 0.38f)
+            )
+        }
+    }
+    particles.forEach { it.updateProgress(progress) }
+
+    Canvas(
+        modifier = Modifier
+            .border(width = 1.dp, color = Color(0x26000000))
+            .size(sizeDp)
+    ) {
+        drawLine(
+            color = Color.Black,
+            start = Offset(sizePxHalf, 0f),
+            end = Offset(sizePxHalf, sizePx),
+            strokeWidth = 2.dp.toPx()
+        )
+        drawLine(
+            color = Color.Black,
+            start = Offset(0f, sizePxHalf),
+            end = Offset(sizePx, sizePxHalf),
+            strokeWidth = 2.dp.toPx()
+        )
+        particles.forEach { particle ->
+            drawCircle(
+                alpha = particle.alpha,
+                color = particle.color,
+                radius = particle.currentRadius,
+                center = Offset(particle.currentXPosition, particle.currentYPosition),
+            )
+        }
+    }
+}
+@Preview
+@Composable
+fun ControlledExplosionPreview() {
+    LearningComposeTheme {
+        ControlledExplosion()
+    }
+}
